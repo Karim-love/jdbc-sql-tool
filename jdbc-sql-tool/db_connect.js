4fd6ee122ca3e5a14513cd6bd70837d4d63065c0
수빈 <sblim@interezen.co.kr>
2021-11-19 17:00:08 +0900

    1. main.html 틀

---
+++ jdbc-sql-tool/db_connect.js
@@ -0,0 +1,5 @@
+const getJdbcConnectValueInput = () =>{
+    let jdbcUrlValue = document.getElementById("jdbcUrlValue").value; 
+    let jdbcIdValue = document.getElementById("jdbcIdValue").value; 
+    let jdbcPwValue = document.getElementById("jdbcPwValue").value; 
+}
